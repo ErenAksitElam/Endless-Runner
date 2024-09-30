8b77edcbc722f4224dbed52e8a2be887d7a8215f
ErenAksitElam <EAksit2024@elam.co.uk>
2024-09-30 23:18:37 +0100

    Scrap Extra Lives

--- Assets/Scripts/Death Zones/DeathZone.cs
+++ Assets/Scripts/Death Zones/DeathZone.cs
@@ -5,48 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class DeathZone : MonoBehaviour
 {
-    public float lives = 1f;
-    public int timer = 30;
-    public float newYPosition = 10f;
-
-
-    private void Start()
-    {
-        InvokeRepeating("IncreaseLives", timer, timer);
-    }
-
-    private void IncreaseLives()
-    {
-        lives += 1f;
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (lives >= 2f)
-            {
-                GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-                if (player == null)
-                {
-                    Transform playerTransform = player.transform;
-
-                    Vector2 newPosition = playerTransform.position;
-                    newPosition.y = newYPosition;
-                    
-                    playerTransform.position = newPosition;
-                }
-                else
-                {
-                    Debug.LogError("Player not found!");
-                }
-            }
-            else
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
